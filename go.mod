4f4616eef1a21784ecdaec53e214adf9b9be7e5c
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-10-14 23:17:54 +0300

    add search front

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/antikuz/demo-avia
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	templatesList, err := filepath.Glob("templates/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(templatesList)
+	templates := template.Must(template.ParseFiles(templatesList...))
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			r.ParseForm()
+			fmt.Println(r.PostForm)
+		}
+		if r.Method == "GET" {
+			if err := templates.ExecuteTemplate(w, "main.html", nil); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			r.ParseForm()
+			fmt.Println(r.PostForm)
+			result := []string{
+				"first line",
+				"second line",
+			}
+			if err := templates.ExecuteTemplate(w, "search.html", result); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	})
+
+	fmt.Println("Listen http://localhost:8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+	
+}
